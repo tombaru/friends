ac9616e144638411a730b6d37a82fc7979e016a7
Pasha <goblin@goblin.(none)>
2014-10-02 18:40:22 +0400

    create simple views for create videos

--- app.rb
+++ app.rb
@@ -5,9 +5,31 @@ require 'json'
 set :database, 'postgres://goblin:123@localhost/friends'
 
 class Video < ActiveRecord::Base
-
 end
 
 get '/' do
-  "Hello World!"
+	"Hello World!"
+end
+
+get '/videos/new' do
+	haml :'videos/new'
+end
+
+get '/videos/:id' do
+	@video = Video.find(params[:id])
+	haml :'videos/show'
+	# 1. find video with id
+end
+
+
+post "/videos/create" do
+
+  video = Video.new(params[:video])
+
+  if video.save
+    redirect to("/videos/#{video.id}")
+  else
+   	redirect to('/videos/new')
+  end
+
 end
