7fc8509b41eaef4124fe6db8659d5c4463dc6233
Pasha <goblin@goblin.(none)>
2014-10-09 20:17:32 +0400

    add file column to videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929142912) do
+ActiveRecord::Schema.define(version: 20141009140726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140929142912) do
     t.string  "title"
     t.integer "episode"
     t.integer "season"
+    t.string  "file"
   end
 
 end
