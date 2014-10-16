5c5c6a7be0e1994b375219b2e58c760cf0bba7e4
Pasha <goblin@goblin.(none)>
2014-10-16 18:14:46 +0400

    add error stream audio count and gem sinatra reloaded

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/reloader'
 require 'json'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
@@ -19,15 +20,23 @@ set :database, 'postgres://goblin:123@localhost/friends'
 class Video < ActiveRecord::Base
   mount_uploader :file, FileUploader
 
-  validates :title, :episode, :season, presence: true
+  validates :title, :episode, :season, :file, presence: true
   validates :episode, uniqueness: { scope: :season }
 
-  before_save :capitalize
+  before_save :check_audio_streams
  
   private
 
   def capitalize
     self.title = title.capitalize
+  end
+  
+  def check_audio_streams
+    string = `ffmpeg -i #{self.file.path}`
+    if string.scan(/Audio/).count != 2
+      errors.add(:file, "audio streams != 2")
+      false 
+    end
   end
 end
 
@@ -49,7 +58,7 @@ end
 post "/videos/create" do
   @video = Video.new(params[:video])
   if @video.save
-    redirect to("/videos/#{video.id}")
+    redirect to("/videos/#{@video.id}")
   else
    	haml :'videos/new', locals: { video: @video }
   end
